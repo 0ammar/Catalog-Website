14f3b482f52961038fce431b29d6fbb9a7d26166
Ammar Arab <132087789+0ammar@users.noreply.github.com>
2025-06-02 15:04:30 +0300

    change23

--- src/app/favourite-items/page.tsx
+++ src/app/favourite-items/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import { useState } from 'react';
 import { useFavouriteItems } from '@/Hooks';
 import { ItemsPage, Searchbar } from '@/Components/Layout';
 import { EmptyState, Pagination } from '@/Components/UI';
 import Loading from '@/Components/UI/Loading/Loading';
+import Modal from '@/Components/UI/Modal/Modal';
 import { Trash2 } from 'lucide-react';
 import '@/styles/globals.scss';
-
 
 const FavouriteItemsPage = () => {
   const {
@@ -20,6 +21,8 @@ const FavouriteItemsPage = () => {
     total,
     clearFavourites,
   } = useFavouriteItems();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   if (error) {
     return <EmptyState title="حدث خطأ" message={error} />;
@@ -35,13 +38,12 @@ const FavouriteItemsPage = () => {
           setQuery(val);
         }}
       />
+
       {total > 0 && (
-        <div style={{ display: 'flex', flexDirection: "column", justifyContent: 'center', margin: '12px 0' }}>
-          <h1 className="title">
-            قائمة المفضلة
-          </h1>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', margin: '12px 0' }}>
+          <h1 className="title">قائمة المفضلة</h1>
           <button
-            onClick={clearFavourites}
+            onClick={() => setIsModalOpen(true)}
             style={{
               background: 'none',
               border: 'none',
@@ -49,7 +51,8 @@ const FavouriteItemsPage = () => {
               padding: '8px',
               borderRadius: '8px',
               transition: '0.2s',
-              color: "#A01515"
+              color: '#A01515',
+              alignSelf: 'flex-start'
             }}
             title="تفريغ المفضلة"
           >
@@ -86,6 +89,17 @@ const FavouriteItemsPage = () => {
           onPageChange={setPage}
         />
       )}
+
+      <Modal
+        isOpen={isModalOpen}
+        title="تأكيد الحذف"
+        message="هل أنت متأكد أنك تريد تفريغ قائمة المفضلة بالكامل؟"
+        onConfirm={() => {
+          clearFavourites();
+          setIsModalOpen(false);
+        }}
+        onCancel={() => setIsModalOpen(false)}
+      />
     </>
   );
 };
